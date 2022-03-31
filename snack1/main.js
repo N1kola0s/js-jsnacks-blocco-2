f93b07a02d53d2bb4b0a898677492e6472d5876a
NicolaMigliore <auster.mig@gmail.com>
2022-03-31 12:49:24 +0200

    add snack1

---
+++ snack1/main.js
@@ -0,0 +1,30 @@
+/* SNACK 1 
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while. */
+
+let num;
+let sum = 0;
+
+/* let i = 0
+
+while(i<5){
+    num = Number(prompt('Inserisci un numero'));
+
+    sum += num;
+
+    i++
+}
+
+console.log(`la somma di tutti i numeri è ${sum}`); */
+
+
+for (let i = 1; i<=5; i++){
+
+    num = Number(prompt('Inserisci un numero'));
+
+    sum += num;
+
+}
+
+console.log(`la somma di tutti i numeri è ${sum}`);
